fac5ffb0c1da42eccb2e2234cf2e0b53c669e745
HugoZAuad <hugozeymer@hotmail.com>
2025-06-15 00:10:09 -0300

    first commit

---
+++ BACKEND/src/app.ts
@@ -0,0 +1,38 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import 'express-async-errors';
+import { AppDataSource } from './config/database';
+import BooksRoutes from './routes/BooksRoutes';
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar com o banco de dados:', error);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/livros', BooksRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API de Livros funcionando!');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: err.message || 'Erro interno do servidor' });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
